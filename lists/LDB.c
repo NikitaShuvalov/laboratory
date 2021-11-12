06a60f8162cd7811aec7f13a088425eb6fe49667
NikitaShuvalov <ktototam065@gmail.com>
2021-11-13 00:37:42 +0600

    using function for adding items in list with index.

--- lists/LDB.c
+++ lists/LDB.c
@@ -1,20 +1,23 @@
 // LDB - List DataBase
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "db.h"
+#include "core.c"
 
-int main()
-{
-	ListDataBase *list_data_base = create("test1");
+int main() {
 
-	addElement(&list_data_base, "test2");
+	ListDataBase *list_data_base = create(0, "test1"); // creating the list.
 
-	while (list_data_base != NULL)
-	{
-		printf("value: %s\n", list_data_base->value);
+        addElement(&list_data_base, 1, "test2"); // using function for adding item in list.
+
+	addElement(&list_data_base, 2, "test3");
+
+	// ....
+
+	while (list_data_base != NULL) {
+
+		printf("index: %d, value: %s\n", list_data_base->index, list_data_base->value);
 		list_data_base = list_data_base->next;
 	}
 
 	return 0;
 }
+
