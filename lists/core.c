c98beeb610757e6747cebe04df7545f3b7a4ca2f
NikitaShuvalov <ktototam065@gmail.com>
2021-12-09 19:21:11 +0600

    write functions to find element by index and find index by element.

--- lists/core.c
+++ lists/core.c
@@ -4,18 +4,17 @@
 #include <stdlib.h>
 
 typedef struct ListDataBase {
-
 	int index;
 	char *value;
 
 	struct ListDataBase *next;
 } ListDataBase;
 
+int list_item_index = 0;
 
-ListDataBase *create(int set_index, char *set_value) { // args: int set_index, char *set_value.
-
+ListDataBase *create(int set_index, char *set_value) {
 	ListDataBase *node = (ListDataBase *)malloc(sizeof(ListDataBase));
-
+	
 	node->index = set_index;
 
 	node->value = set_value;
@@ -28,10 +27,38 @@ ListDataBase *create(int set_index, char *set_value) { // args: int set_index, c
 
 // Function for adding items in list.
 void addElement(ListDataBase **list_data_base, int set_index, char *set_value) {
-
+	// list_item_index++;
+	
 	ListDataBase *new_element = create(set_index, set_value);
 
 	new_element->next = *list_data_base;
 	*list_data_base = new_element;
 }
 
+
+// Function that returns the item at its index in the list.
+void readDatabase(ListDataBase **list_data_base, int index) {
+	ListDataBase *node = (ListDataBase *)malloc(sizeof(ListDataBase));
+
+	node->next = *list_data_base;
+
+	while (node->next != NULL) {
+		if (node->index == index) {
+			printf("Value by index %d: %s\n", index, node->value);
+		}
+	}
+}
+
+
+// Function that returns the index of an element in the list.
+void searchElementInDatabase(ListDataBase **list_data_base, char value) {
+	ListDataBase *node = (ListDataBase *)malloc(sizeof(ListDataBase));
+
+	node->next = *list_data_base;
+
+	while (node->next != NULL) {
+		if (node->value == value) {
+			printf("Index value %s is: %d\n", value, node->index);
+		}
+	}
+}
