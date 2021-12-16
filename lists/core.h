52401555fde5dc6debf10e1b7769dc407fca6e58
NikitaShuvalov <ktototam065@gmail.com>
2021-12-17 02:47:36 +0600

    header file.

---
+++ lists/core.h
@@ -0,0 +1,10 @@
+// headers file.
+
+// function for adding items in list.
+void add_element_in_database(ListDataBase **list_data_base, int set_index, char *set_value);
+
+// function that returns the item at its index in the list.
+void find_element_in_database(ListDataBase **list_data_base, int index);
+
+// function that returns the index of an element in the list.
+void find_index_in_database(ListDataBase **list_data_base, char *value);
